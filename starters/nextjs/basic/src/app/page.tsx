55b50c81fa7b0440e5a491b0f18e49c316e3917a
darkthecross <darkthecross@gmail.com>
2025-11-06 01:20:18 -0800

    Minor, trigger first rollout I guess?

--- starters/nextjs/basic/src/app/page.tsx
+++ starters/nextjs/basic/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="content">
       <h1 className="heading">Next.js on Firebase App Hosting</h1>
+      <h1>WTF, so complicated...</>
       <p>{message}</p>
 
       <section className="features">
